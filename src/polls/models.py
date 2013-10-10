05c201fc9c6154cd6b7faaa457f14e1cfe3d165f
roddiedude <roddiedude@gmail.com>
2013-10-10 18:04:13 +0530

    Testing, static content, generic views

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,6 +1,9 @@
 import datetime
 from django.utils import timezone
 from django.db import models
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your models here.
 class Poll(models.Model):
@@ -11,7 +14,14 @@ class Poll(models.Model):
         return self.question
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
+        now = timezone.now()
+        print '========================================================='
+        print self.pub_date
+        print now - datetime.timedelta(days=1) <= self.pub_date < now
+        
+        return now - datetime.timedelta(days=1) <= self.pub_date <  now
     
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
--- src/polls/views.py
+++ src/polls/views.py
@@ -1,11 +1,15 @@
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 from django.core import serializers
 from django.core.serializers.json import DjangoJSONEncoder
-import json
+
+from django.core.urlresolvers import reverse
+
+from django.views import generic
+from django.utils import timezone
 
 from django.template import RequestContext, loader
 # def index(request):
@@ -17,7 +21,9 @@ from django.template import RequestContext, loader
 #     return HttpResponse(template.render(context));
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]    
+    latest_poll_list = Poll.objects.filter(
+        pub_date___lte=timezone.now()        
+    ).order_by('-pub_date')[:5]
     context = {
         'latest_poll_list' : latest_poll_list
     }
@@ -35,10 +41,56 @@ def detail(request, poll_id):
 
 def results(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)           
-    data = serializers.serialize("json", [poll])
-    #data = json.dumps(poll, cls=DjangoJSONEncoder)
+    #data = serializers.serialize("json", [poll])    
     
-    return HttpResponse(data)
+    return render(request, 'polls/results.html', {'poll':poll})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the poll voting for,
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+    
+class IndexView(generic.ListView):
+    template_name='polls/index.html'
+    context_object_name = 'latest_poll_list'
+    
+    def get_queryset(self):
+        """
+        Return the last five published polls (not including those set to be
+        published in the future).
+        """
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
